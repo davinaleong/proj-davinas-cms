09aeb3af04a6be151a67bdab30cb69c189cb37db
Personal Profile <davina.leong.sy@gmail.com>
2022-04-18 05:55:47 +0800

    Added setting feature test

--- tests/Feature/Cms/SettingTest.php
+++ tests/Feature/Cms/SettingTest.php
@@ -2,12 +2,13 @@
 
 namespace Tests\Feature\Cms;
 
+use App\Models\Activity;
+use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-/** @group new */
 class SettingTest extends TestCase
 {
     use RefreshDatabase;
@@ -42,5 +43,78 @@ class SettingTest extends TestCase
         $this->actingAs($user)
             ->get('cms/settings/edit')
             ->assertOk();
+    }
+
+    public function test_guest_cannot_store_settings()
+    {
+        $this->get('cms/settings')
+            ->assertStatus(302)
+            ->assertRedirect('/login');
+    }
+
+    public function test_user_can_store_settings()
+    {
+        $user = User::factory()->create();
+        $settings = Setting::factory(3)->make();
+        $activity = Activity::factory()->make([
+            'message' => 'Settings modified.',
+            'label' => 'View Settings',
+            'link' => route('settings.index')
+        ]);
+
+        $names = [];
+        $keys = [];
+        $values = [];
+
+        foreach ($settings as $setting) {
+            $names[] = $setting->name;
+            $keys[] = $setting->key;
+            $values[] = $setting->value;
+        }
+
+        $response = $this->actingAs($user)
+            ->post('cms/settings', [
+                'names' => $names,
+                'keys' => $keys,
+                'values' => $values,
+            ]);
+
+        $response->assertStatus(302);
+        $response->with('message', 'Settings modified.');
+
+        foreach ($settings as $setting) {
+            $this->assertDatabaseHas('settings', [
+                'user_id' => $user->id,
+                'name' => $setting->name,
+                'key' => $setting->key,
+                'value' => $setting->value
+            ]);
+        }
+
+        $this->assertDatabaseHas('activities', [
+            'user_id' => $user->id,
+            'message' => $activity->message,
+            'label' => $activity->label,
+            'link' => $activity->link,
+        ]);
+    }
+
+    public function test_store_settings_validation()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->post('cms/settings', [
+                'names' => [],
+                'keys' => [],
+                'values' => [],
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors([
+            'names',
+            'keys',
+            'values',
+        ]);
     }
 }
