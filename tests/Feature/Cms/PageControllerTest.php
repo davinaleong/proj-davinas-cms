080a9db89af12f4cda6c266593b4d4e86ba3b003
Personal Profile <davina.leong.sy@gmail.com>
2022-04-18 11:44:55 +0800

    Removed annotation

--- tests/Feature/Cms/PageControllerTest.php
+++ tests/Feature/Cms/PageControllerTest.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-/** @group new */
 class PageControllerTest extends TestCase
 {
     use RefreshDatabase;
